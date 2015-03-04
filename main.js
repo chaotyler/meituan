a0784b63ecd694d5098618a2dcefe1102f216220
Tyler <ymjring@gmail.com>
2015-03-04 22:35:03 +0800

    小改动

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-var arr = document.getElementById("input").value.split(";");
-var resultArea = document.getElementById("result");
-var length = arr.length;
-var result = [];
-var x, y;
-//获取输入字符串并转换成二维数组
-for(var i = 0; i < arr.length; i ++){
-    arr[i] = arr[i].split(",");
-}
-//横向从右向左
-for(var i = length - 1; i >= 0; i --){
-    x = 0;
-    y = i;
-    while(y < length){
-        result.push(arr[x][y]);
-        x ++;
-        y ++;
+out();
+document.getElementById("outBtn").addEventListener("click", out, false);
+function out() {
+    var arr = document.getElementById("input").value.split(";");
+    var resultArea = document.getElementById("result");
+    var length = arr.length;
+    var result = [];
+    var x, y;
+    //获取输入字符串并转换成二维数组
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].split(",");
     }
-}
-//竖向从上向下
-for(var j = 1; j < length; j ++){
-    x = j;
-    y = 0;
-    while(x < length){
-        result.push(arr[x][y]);
-        x ++;
-        y ++;
+    //横向从右向左
+    for (var i = length - 1; i >= 0; i--) {
+        x = 0;
+        y = i;
+        while (y < length) {
+            result.push(arr[x][y]);
+            x++;
+            y++;
+        }
     }
+    //竖向从上向下
+    for (var j = 1; j < length; j++) {
+        x = j;
+        y = 0;
+        while (x < length) {
+            result.push(arr[x][y]);
+            x++;
+            y++;
+        }
+    }
+    resultArea.value = result.join(",");
 }
-resultArea.innerHTML = result.join(",");
